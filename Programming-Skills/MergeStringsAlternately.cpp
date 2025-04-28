8b835a063c0e5b6f52fcca3e5cc8bcef370def87
SovanCode <208999657+Kalacade@users.noreply.github.com>
2025-04-28 20:02:40 +0200

    Create MergeStringsAlternately.cpp

---
+++ Programming-Skills/MergeStringsAlternately.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    string word1 = "abc";
+    string word2 = "pqr";
+    string result = solution.mergeAlternately(word1, word2);
+    cout << "Merged string: " << result << endl;
+    return 0;
+}

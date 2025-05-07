41c69d6707dc6ba2357699b7458b5f46414b9d16
SovanCode <bypass.kosei@gmail.com>
2025-05-07 21:55:34 +0200

    Update spiralMatrix.cpp

--- Programming-Skills/spiralMatrix.cpp
+++ Programming-Skills/spiralMatrix.cpp
@@ -6,12 +6,46 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        int haut = 0, bas = matrix.size() - 1, gauche = 0, droite = matrix[0].size() - 1;
+        vector<int> result;
+
+        while (haut <= bas && gauche <= droite) {
+          
+            for (int i = gauche; i <= droite; i++) {
+                result.push_back(matrix[haut][i]);
+            }
+            haut++;  
+            
+            
+            for (int i = haut; i <= bas; i++) {
+                result.push_back(matrix[i][droite]);
+            }
+            droite--;  
+            
+            if (haut <= bas) {
+                
+                for (int i = droite; i >= gauche; i--) {
+                    result.push_back(matrix[bas][i]);
+                }
+                bas--;  
+            }
+            
+            if (gauche <= droite) {
+                
+                for (int i = bas; i >= haut; i--) {
+                    result.push_back(matrix[i][gauche]);
+                }
+                gauche++;  
+            }
+        }
+        return result;
     }
 };
 
 int main() {
     Solution solution;
-    
+
     vector<vector<int>> test1 = {{1,2,3},{4,5,6},{7,8,9}};
     cout << "Test 1: [[1,2,3],[4,5,6],[7,8,9]]" << endl;
     cout << "Expected: [1,2,3,6,9,8,7,4,5]" << endl;
